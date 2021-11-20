7832bbedf4727d21bf69d89351ed053733f6c733
Mafinar Khan <mafinar@gmail.com>
2021-11-19 23:20:08 -0500

    Add nice deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule ExAlgo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:benchee, "~> 1.0", only: :dev},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:stream_data, "~> 0.5", only: [:dev, :test]}
     ]
   end
 end
