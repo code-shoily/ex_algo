4c6bef0e52fbb9295acc68fc7518e1cf6ac3f0b2
Mafinar Khan <mafinar@gmail.com>
2021-11-21 17:30:04 -0500

    Added some more functions for LL

--- lib/ex_algo/list/linked_list.ex
+++ lib/ex_algo/list/linked_list.ex
@@ -2,6 +2,8 @@ defmodule ExAlgo.List.LinkedList do
   @moduledoc """
   Implementation of a singly linked list.
   """
+  @type neg_index_error :: {:error, :negative_index}
+  @type empty_error :: {:error, :empty_list}
   @type value_type :: any()
   @type t :: %__MODULE__{container: [value_type()]}
 
@@ -63,7 +65,59 @@ defmodule ExAlgo.List.LinkedList do
     {:error, :empty_list}
 
   """
-  @spec remove(t()) :: {value_type(), t()} | {:error, :empty_list}
+  @spec remove(t()) :: {value_type(), t()} | empty_error()
   def remove(%__MODULE__{container: []}), do: {:error, :empty_list}
   def remove(%__MODULE__{container: [head | rest]}), do: {head, %__MODULE__{container: rest}}
+
+  @doc """
+  Returns the head of the linked list
+
+  ## Example
+
+      iex> alias ExAlgo.List.LinkedList
+      iex> LinkedList.from(1..10) |> LinkedList.head()
+      1
+
+      iex> alias ExAlgo.List.LinkedList
+      iex> LinkedList.new |> LinkedList.head()
+      {:error, :empty_list}
+
+  """
+  @spec head(t()) :: empty_error()
+  def head(%__MODULE__{container: [head | _]}), do: head
+  def head(_), do: {:error, :empty_list}
+
+  @doc """
+  Returns the tail of the linked list
+
+  ## Example
+
+      iex> alias ExAlgo.List.LinkedList
+      iex> LinkedList.from(1..3) |> LinkedList.tail()
+      [2, 3]
+
+      iex> alias ExAlgo.List.LinkedList
+      iex> LinkedList.new |> LinkedList.tail()
+      {:error, :empty_list}
+
+  """
+  @spec tail(t()) :: empty_error()
+  def tail(%__MODULE__{container: [_ | tail]}), do: tail
+  def tail(_), do: {:error, :empty_list}
+
+  @doc """
+  Return the element at index. Index is 0 based and must be positive. Errors on empty list.
+
+  ## Example
+
+      iex> alias ExAlgo.List.LinkedList
+      iex> LinkedList.from(0..10) |> LinkedList.at(3)
+      3
+
+  """
+  @spec at(t(), value_type()) :: value_type() | empty_error() | neg_index_error()
+  def at(%__MODULE__{container: []}, _), do: {:error, :empty_list}
+  def at(_, index) when index < 0, do: {:error, :negative_index}
+  def at(list, 0), do: list |> head()
+  def at(list, index), do: list |> tail |> from |> at(index - 1)
 end
--- test/ex_algo/list/linked_list_test.exs
+++ test/ex_algo/list/linked_list_test.exs
@@ -1,8 +1,87 @@
 defmodule ExAlgo.List.LinkedListTest do
   use ExUnit.Case
+  use ExUnitProperties
   @moduletag :linked_list
 
   doctest ExAlgo.List.LinkedList
 
   alias ExAlgo.List.LinkedList
+
+  setup_all do
+    {:ok, %{
+      empty_list: LinkedList.new(),
+      list: LinkedList.from(1..10)
+    }}
+  end
+
+  describe "new/0" do
+    test "creates an empty linked list" do
+      assert %LinkedList{container: []} == LinkedList.new()
+    end
+  end
+
+  describe "from/1" do
+    property "The list that is passed in is the container of the created list" do
+      check all list <- list_of(term()) do
+        %LinkedList{container: container} = LinkedList.from(list)
+        assert container == list
+      end
+    end
+  end
+
+  describe "insert/2" do
+    test "insert an element in an empty list", %{empty_list: list} do
+      assert %LinkedList{container: [0]} == list |> LinkedList.insert(0)
+    end
+
+    test "insert an element at the head of a list", %{list: list} do
+      %LinkedList{container: container} = list |> LinkedList.insert(0)
+      assert container == Enum.to_list(0..10)
+    end
+  end
+
+  describe "remove/1" do
+    test "error when trying to remove from an empty list", %{empty_list: list} do
+      assert {:error, :empty_list} == list |> LinkedList.remove()
+    end
+
+    test "remove the head of the list", %{list: list} do
+      {value, %LinkedList{container: container}} = list |> LinkedList.remove()
+      assert value == hd(list.container)
+      assert container == tl(list.container)
+    end
+  end
+
+  describe "head/1" do
+    property "head always returns the head of the underlying list" do
+      check all list <- nonempty list_of(term()) do
+        linked_list = LinkedList.from(list)
+        assert LinkedList.head(linked_list) == hd(list)
+      end
+    end
+  end
+
+  describe "tail/1" do
+    property "tail always returns the tail of the underlying list" do
+      check all list <- nonempty list_of(term()) do
+        linked_list = LinkedList.from(list)
+        assert LinkedList.tail(linked_list) == tl(list)
+      end
+    end
+  end
+
+  describe "at/2" do
+    test "cannot index an empty list", %{empty_list: list} do
+      assert {:error, :empty_list} == LinkedList.at(list, 0)
+      assert {:error, :empty_list} == LinkedList.at(list, 1)
+    end
+
+    test "cannot use negative index", %{list: list} do
+      assert {:error, :negative_index} == LinkedList.at(list, -1)
+    end
+
+    test "when querying an empty list with negative index empty error is returned", %{empty_list: list} do
+      assert {:error, :empty_list} == LinkedList.at(list, -1)
+    end
+  end
 end
