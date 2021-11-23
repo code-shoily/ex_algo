ff8004964e4147eafce2db4cd71d31ce2242d41f
Mafinar Khan <mafinar@gmail.com>
2021-11-23 07:19:43 -0500

    Implement insertion sort

---
+++ lib/ex_algo/sort/insertion.ex
@@ -0,0 +1,36 @@
+defmodule ExAlgo.Sort.Insertion do
+  @moduledoc """
+  Implements sorting algorithms based on insertion.
+  """
+  @type item :: any()
+  @type t :: [item()]
+
+  @doc """
+  Perform sort by using the merge_sort algorithm.
+
+  ## Example
+
+      iex> import ExAlgo.Sort.Insertion
+      iex> insertion_sort([])
+      []
+      iex> insertion_sort([1])
+      [1]
+      iex> insertion_sort([3, 2, 1])
+      [1, 2, 3]
+      iex> insertion_sort([2, 7, -1, 5])
+      [-1, 2, 5, 7]
+
+  """
+  @spec insertion_sort(t) :: t
+  def insertion_sort([]), do: []
+  def insertion_sort([head | rest]) do
+    rest
+    |> insertion_sort()
+    |> insert(head)
+  end
+
+  @spec insert(t, item) :: t
+  defp insert([], item), do: [item]
+  defp insert([head | tail], item) when head < item, do: [head | insert(tail, item)]
+  defp insert(sorted_list, item), do: [item | sorted_list]
+end
---
+++ test/ex_algo/sort/insertion_test.exs
@@ -0,0 +1,17 @@
+defmodule ExAlgo.Sort.InsertionTest do
+	use ExUnit.Case
+  use ExUnitProperties
+  @moduletag :insertion_sorts
+
+	doctest ExAlgo.Sort.Insertion
+
+	alias ExAlgo.Sort.Insertion
+
+  describe "insertion_sort/1" do
+    property "ensure insertion_sort done by any list produces the same result as enum sort" do
+      check all list <- list_of(integer()) do
+        assert Insertion.insertion_sort(list) == Enum.sort(list)
+      end
+    end
+  end
+end
