7a9c3980b7a4ad98d22728206d695c3e27c60550
Mafinar Khan <mafinar@gmail.com>
2021-11-23 07:10:50 -0500

    Add test for merge sort

---
+++ test/ex_algo/sort/merge_test.exs
@@ -0,0 +1,17 @@
+defmodule ExAlgo.Sort.MergeTest do
+	use ExUnit.Case
+  use ExUnitProperties
+  @moduletag :merge_sorts
+
+	doctest ExAlgo.Sort.Merge
+
+	alias ExAlgo.Sort.Merge
+
+  describe "merge_sort/1" do
+    property "ensure merge_sort done by any list produces the same result as enum sort" do
+      check all list <- list_of(integer()) do
+        assert Merge.merge_sort(list) == Enum.sort(list)
+      end
+    end
+  end
+end
