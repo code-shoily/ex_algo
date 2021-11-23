2e818a0baff0d727ec2051d3541093e0949e7a95
Mafinar Khan <mafinar@gmail.com>
2021-11-23 07:36:30 -0500

    Add delegates to sort

--- lib/ex_algo/sort/sort.ex
+++ lib/ex_algo/sort/sort.ex
@@ -2,4 +2,9 @@ defmodule ExAlgo.Sort do
   @moduledoc """
   Implementation of various sorting algorithms.
   """
+  alias __MODULE__.{Exchange, Insertion, Merge}
+
+  defdelegate insertion_sort(list), to: Insertion, as: :insertion_sort
+  defdelegate merge_sort(list), to: Merge, as: :merge_sort
+  defdelegate quick_sort(list), to: Exchange, as: :quick_sort
 end
