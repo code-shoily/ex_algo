92de051e1e8c1fa7a6968c49dd5545f446d3f630
Mafinar Khan <mafinar@gmail.com>
2021-11-25 20:19:17 -0500

    Add selection sort to sort module

--- lib/ex_algo/sort/sort.ex
+++ lib/ex_algo/sort/sort.ex
@@ -2,11 +2,12 @@ defmodule ExAlgo.Sort do
   @moduledoc """
   Implementation of various sorting algorithms.
   """
-  alias __MODULE__.{Distribution, Exchange, Insertion, Merge}
+  alias __MODULE__.{Distribution, Exchange, Insertion, Merge, Selection}
 
   defdelegate bubble_sort(list), to: Exchange, as: :bubble_sort
   defdelegate insertion_sort(list), to: Insertion, as: :insertion_sort
   defdelegate merge_sort(list), to: Merge, as: :merge_sort
   defdelegate pigeonhole_sort(list), to: Distribution, as: :pigeonhole_sort
   defdelegate quick_sort(list), to: Exchange, as: :quick_sort
+  defdelegate selection_sort(list), to: Selection, as: :selection_sort
 end
